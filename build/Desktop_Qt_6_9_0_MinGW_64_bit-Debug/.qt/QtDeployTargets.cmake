fa3d3080adca836502b14f2738af464879e84ee6
Yam1noko <pasha.yavits@yandex.ru>
2025-05-15 20:53:12 +1000

    Let's begin

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_File_manager_FILE C:/Users/Jason/Documents/projects/File_manager/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/File_manager.exe)
+set(__QT_DEPLOY_TARGET_File_manager_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_File_manager_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
