05f2a70da912c74a6dc6794b60bb6c85cf1805dd
Yam1noko <pasha.yavits@yandex.ru>
2025-05-19 17:38:39 +1000

    Added display change for file list and synchronization with the tree

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -1,15 +1,21 @@
 #include "Directory.h"
 #include <QDir>
 #include <QFile>
+#include <qboxlayout.h>
 
 
 
-Directory::Directory(QTreeView* treeView, QListView* listView, QObject *parent)
+Directory::Directory(QTreeView* treeView, QListView* listView,
+                     QPushButton* backButton, QPushButton* forwardButton,
+                     QPushButton* ListGridButton, QObject *parent)
     : QObject(parent),
     folderModel(new QFileSystemModel(this)),
     fileModel(new QFileSystemModel(this)),
     treeView(treeView),
-    listView(listView)
+    listView(listView),
+    backButton(backButton),
+    forwardButton(forwardButton),
+    ListGridButton(ListGridButton)
 {
     setup();
 }
@@ -23,13 +29,21 @@ void Directory::setup(){
     treeView->setModel(folderModel);
     treeView->setRootIndex(folderModel->index(""));
     treeView->setHeaderHidden(true);
+    for (int i = 1; i < folderModel->columnCount(); ++i)
+        treeView->hideColumn(i);
 
     fileModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs | QDir::Files | QDir::Hidden);
     fileModel->setRootPath("");
     listView->setModel(fileModel);
     listView->setRootIndex(fileModel->index(""));
+    listView->setViewMode(QListView::IconMode);
+    listView->setGridSize(QSize(100, 100));
+    listView->setIconSize(QSize(64, 64));
+    listView->setResizeMode(QListView::Adjust);
+    listView->setMovement(QListView::Static);
 
-    connect(treeView->selectionModel(), &QItemSelectionModel::currentChanged, this, [=](const QModelIndex &current, const QModelIndex &){
+    connect(treeView->selectionModel(), &QItemSelectionModel::currentChanged,
+            this, [=](const QModelIndex &current, const QModelIndex &){
         QString path = folderModel->filePath(current);
         listView->setRootIndex(fileModel->setRootPath(path));
     });
@@ -49,5 +63,45 @@ void Directory::setup(){
         }
     });
 
+    connect(ListGridButton, &QPushButton::clicked, this, [=](){
+        if (listView->viewMode() == QListView::ViewMode::IconMode){
+            listView->setViewMode(QListView::ListMode);
+            listView->setGridSize(QSize(32, 32));
+            listView->setIconSize(QSize(32, 32));
+        } else{
+            listView->setViewMode(QListView::IconMode);
+            listView->setGridSize(QSize(100, 100));
+            listView->setIconSize(QSize(64, 64));
+        }
+    });
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
